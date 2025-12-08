b302ebef85988f6203698d234180f59c38fdf0e9
zaynahahmer <zaynah.ahmer@mail.utoronto.ca>
2025-12-08 16:48:09 -0500

    created constructor

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -51,13 +51,16 @@ public class JSONTranslator implements Translator {
 
                 List<String> languages = new ArrayList<>();
 
-                // TODO Task C: record this countryCode in the correct instance variable
+                this.countryCodes.add(countryCode);
 
                 // iterate through the other keys to get the information that we need
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
-                        // TODO Task C: record this translation in the appropriate instance variable
+
+                        this.languageCodes.add(languageCode);
+                        this.translations.put(countryCode + '_' + languageCode, countryData.getString(key));
+
 
                         if (!languages.contains(languageCode)) {
                             languages.add(languageCode);
